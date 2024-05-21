1beeea4651084c1bda9f4ac253dfca1687ad3e99
Amila Abeygunasekara <auabey@gmail.com>
2024-05-21 00:38:37 -0400

    Change background color to dark gray

--- LuminaEngine/main.cpp
+++ LuminaEngine/main.cpp
@@ -100,7 +100,7 @@ void renderLoop(GLFWwindow* window)
 
     displayUI();
 
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     backpackShader->use();
